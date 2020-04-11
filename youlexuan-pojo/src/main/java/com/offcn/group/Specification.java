7af64b26011b2263178ec7f9b46fdb62983a200a
fszzuli <1291503505@qq.com>
2020-04-11 19:04:46 +0800

    good

---
+++ youlexuan-pojo/src/main/java/com/offcn/group/Specification.java
@@ -0,0 +1,58 @@
+package com.offcn.group;
+
+import com.offcn.pojo.TbSpecification;
+import com.offcn.pojo.TbSpecificationOption;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @ClassNameSpecification
+ * @Description TODO
+ * Auther FSZZULI
+ * @Date2020/4/9 21:29
+ * @Version 1.0
+ *
+ * 后台组合实体类
+ *  1.规格
+ *  2.规格项
+ **/
+public class Specification implements Serializable {
+
+    private TbSpecification specification;
+
+    //this
+    List<TbSpecificationOption> specificationOptionList;
+
+    public TbSpecification getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(TbSpecification specification) {
+        this.specification = specification;
+    }
+
+    public List<TbSpecificationOption> getSpecificationOptionList() {
+        return specificationOptionList;
+    }
+
+    public void setSpecificationOptionList(List<TbSpecificationOption> specificationOptionList) {
+        this.specificationOptionList = specificationOptionList;
+    }
+
+    public Specification(TbSpecification specification, List<TbSpecificationOption> specificationOptionList) {
+        this.specification = specification;
+        this.specificationOptionList = specificationOptionList;
+    }
+
+    public Specification() {
+    }
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "specification=" + specification +
+                ", specificationOptionList=" + specificationOptionList +
+                '}';
+    }
+}
